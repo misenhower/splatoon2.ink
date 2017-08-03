2123fe60dde28a0f5ab09711ffcb2f66deda88fc
Matt Isenhower <matt@isenhower.com>
2017-08-02 19:29:55 -0700

    Fix cron time

--- src/splatoon/run.js
+++ src/splatoon/run.js
@@ -4,6 +4,6 @@ const splatnet = require('./splatnet');
 console.info('Starting periodic tasks...');
 
 // Run every day at 2min 30sec after the hour
-new CronJob('02 24 * * * *', () => {
+new CronJob('30 2 * * * *', () => {
     splatnet.update();
 }, null, true);
