29310da3f200d86e89fbeed3a2427c42af1bb6f7
Matt Isenhower <matt@isenhower.com>
2017-08-06 10:16:01 -0700

    Update data closer to the top of the hour
    This will help make Salmon Run times show up sooner.

--- src/splatoon/run.js
+++ src/splatoon/run.js
@@ -3,7 +3,7 @@ const splatnet = require('./splatnet');
 
 console.info('Starting periodic tasks...');
 
-// Run every day at 2min 30sec after the hour
-new CronJob('30 2 * * * *', () => {
+// Run every day at 0min 30sec after the hour
+new CronJob('30 0 * * * *', () => {
     splatnet.update();
 }, null, true);
