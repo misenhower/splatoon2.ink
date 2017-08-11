6ee3e2a5a0e258748a2da2179430fb2867799351
Matt Isenhower <matt@isenhower.com>
2017-08-10 23:33:13 -0700

    Remove an unused div

--- src/js/components/SalmonRunBox.vue
+++ src/js/components/SalmonRunBox.vue
@@ -17,32 +17,30 @@
             </div>
         </div>
 
-        <div class="salmon-run-contentz">
-            <div class="columns is-gapless">
-                <div class="column is-5">
-                    <div class="image is-16by9">
-                        <img src="../../img/salmon-run.png" style="z-index: 1" />
-                    </div>
+        <div class="columns is-gapless">
+            <div class="column is-5">
+                <div class="image is-16by9">
+                    <img src="../../img/salmon-run.png" style="z-index: 1" />
                 </div>
-                <div class="column">
-                    <div class="salmon-run-content">
-                        <div class="is-size-5 title-squid font-splatoon1">
-                            <span v-if="isOpen">Now open!</span>
-                            <span v-else>Soon!</span>
-                        </div>
-                        <div class="title-color is-size-5">
-                            {{ coop.schedule.start_time | date }}
-                            {{ coop.schedule.start_time | time }}
-                            &ndash;
-                            {{ coop.schedule.end_time | date }}
-                            {{ coop.schedule.end_time | time }}
-                        </div>
-                        <div v-if="isOpen">
-                            {{ coop.schedule.end_time - now | duration }} remaining
-                        </div>
-                        <div v-else>
-                            in {{ coop.schedule.start_time - now | duration }}
-                        </div>
+            </div>
+            <div class="column">
+                <div class="salmon-run-content">
+                    <div class="is-size-5 title-squid font-splatoon1">
+                        <span v-if="isOpen">Now open!</span>
+                        <span v-else>Soon!</span>
+                    </div>
+                    <div class="title-color is-size-5">
+                        {{ coop.schedule.start_time | date }}
+                        {{ coop.schedule.start_time | time }}
+                        &ndash;
+                        {{ coop.schedule.end_time | date }}
+                        {{ coop.schedule.end_time | time }}
+                    </div>
+                    <div v-if="isOpen">
+                        {{ coop.schedule.end_time - now | duration }} remaining
+                    </div>
+                    <div v-else>
+                        in {{ coop.schedule.start_time - now | duration }}
                     </div>
                 </div>
             </div>
