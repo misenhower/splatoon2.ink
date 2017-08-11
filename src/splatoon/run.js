081b7dac2ad4724fbf428a52ea9733c2b5046816
Matt Isenhower <matt@isenhower.com>
2017-08-11 12:08:07 -0700

    Add the Salmon Run calendar to the hourly update script

--- src/splatoon/run.js
+++ src/splatoon/run.js
@@ -1,9 +1,11 @@
 const CronJob = require('cron').CronJob;
 const splatnet = require('./splatnet');
+const salmoncalendar = require('./salmoncalendar');
 
 console.info('Starting periodic tasks...');
 
 // Run every day at 0min 30sec after the hour
 new CronJob('30 0 * * * *', () => {
     splatnet.update();
+    salmoncalendar.update();
 }, null, true);
