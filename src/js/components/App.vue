920ee6bd04b47c90401409d6cd74da7e3dcc2c3f
Matt Isenhower <matt@isenhower.com>
2017-09-01 12:52:15 -0700

    Fix a mistake, this was moved elsewhere

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -277,53 +277,6 @@ export default {
                 .then(response => this.salmonruncalendar = response.data)
                 .catch(e => console.error(e));
         },
-        detectRegion() {
-            if (window.navigator && window.navigator.language) {
-                switch (window.navigator.language) {
-                    case 'en-US': // USA
-                    case 'en-CA': // Canada (English)
-                    case 'fr-CA': // Canada (French)
-                    case 'es-MX': // Mexico
-                    case 'en-AU': // Australia
-                    case 'en-NZ': // New Zealand
-                        this.selectedRegionKey = 'na';
-                        break;
-                    case 'de-AT': // Austria
-                    case 'nl-BE': // Belgium (Dutch)
-                    case 'fr-BE': // Belgium (French)
-                    case 'cs-CZ': // Czech Republic
-                    case 'da-DK': // Denmark
-                    case 'de':    // Germany
-                    case 'de-DE': // Germany
-                    case 'es-ES': // Spain
-                    case 'fi-FI': // Finland
-                    case 'fr':    // France
-                    case 'fr-FR': // France
-                    case 'el-GR': // Greece
-                    case 'hu-HU': // Hungary
-                    case 'it-IT': // Italy
-                    case 'nl':    // Netherlands
-                    case 'nl-NL': // Netherlands
-                    case 'nb-NO': // Norway
-                    case 'pl-PL': // Poland
-                    case 'pt-PT': // Portugal
-                    case 'ru-RU': // Russia
-                    case 'en-ZA': // South Africa
-                    case 'sv-SE': // Sweden
-                    case 'de-CH': // Switzerland (German)
-                    case 'fr-CH': // Switzerland (French)
-                    case 'it-CH': // Switzerland (Italian)
-                    case 'en-GB': // United Kingdom
-                    case 'en-IE': // Ireland
-                        this.selectedRegionKey = 'eu';
-                        break;
-                    case 'ja':    // Japan
-                    case 'ja-JP': // Japan
-                        this.selectedRegionKey = 'jp';
-                        break;
-                }
-            }
-        },
         updateNow() {
             this.now = Math.trunc((new Date).getTime() / 1000);
         },
