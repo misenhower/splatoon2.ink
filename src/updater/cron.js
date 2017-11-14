403ea219bd9d04505f1ee60bbd226e8f78a57fcc
Unknown <matt@isenhower.com>
2017-11-13 17:04:43 -0800

    Retrieve updates from SplatNet closer to the top of the hour

--- src/updater/cron.js
+++ src/updater/cron.js
@@ -5,8 +5,8 @@ const twitter = require('./twitter');
 
 console.info('Starting periodic tasks...');
 
-// Run every hour at 30sec after the hour
-new CronJob('30 0 * * * *', async () => {
+// Run every hour at 10sec after the hour
+new CronJob('10 0 * * * *', async () => {
     await update.updateAll();
     await twitter.maybePostTweets();
 }, null, true);
