3f1ca22eafc323065b3e3de7f4f5a1f77031d864
Matt Isenhower <matt@isenhower.com>
2018-03-13 09:31:51 -0700

    Fix Chicken vs. Egg Splatfest images

--- src/updater/updaters/FestivalsUpdater.js
+++ src/updater/updaters/FestivalsUpdater.js
@@ -3,6 +3,7 @@ const mkdirp = require('mkdirp');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const jsonpath = require('jsonpath');
 const { readJson } = require('../utilities');
 const { languages } = require('../../js/regions');
 
@@ -38,6 +39,18 @@ class FestivalsUpdater extends Updater {
     }
 
     processData(regionData) {
+        // Fix alpha/bravo images for the Chicken vs. Egg Splatfest.
+        // For some reason these got swapped out with images that have an opaque background
+        // even though they started out with transparent images.
+        jsonpath.apply(regionData, '$..images.alpha', value => value.replace(
+            "/images/festival/a070cc6b405b4fb335992d824097acd8.png",
+            "/images/festival/06b3b0b7773d9e6c4ac0a5cc5371fc32.png"
+        ));
+        jsonpath.apply(regionData, '$..images.bravo', value => value.replace(
+            "/images/festival/00e4c5fdccd3720d07127084fc1f4152.png",
+            "/images/festival/d93df77468714c6211e9377f39a559f4.png"
+        ));
+
         // Load existing data since we only need to modify this region's data
         let data = {};
         let filename = this.getFilename();
