8dab2a7f605d8f2f86dc20f9ea6a24cb3f61a2b2
Matt Isenhower <matt@isenhower.com>
2023-06-25 17:21:25 -0700

    Use Twitter V2 API

--- src/app/twitter/client.js
+++ src/app/twitter/client.js
@@ -1,39 +1,33 @@
-const Twitter = require('twitter');
+const { TwitterApi } = require("twitter-api-v2");
 
 // Twitter API parameters
-const consumer_key = process.env.TWITTER_CONSUMER_KEY;
-const consumer_secret = process.env.TWITTER_CONSUMER_SECRET;
-const access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY;
-const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+const appKey = process.env.TWITTER_CONSUMER_KEY;
+const appSecret = process.env.TWITTER_CONSUMER_SECRET;
+const accessToken = process.env.TWITTER_ACCESS_TOKEN_KEY;
+const accessSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
 // Twitter API client
-const client = new Twitter({ consumer_key, consumer_secret, access_token_key, access_token_secret });
-module.exports.client = client;
+function getClient() {
+    return new TwitterApi({
+        appKey,
+        appSecret,
+        accessToken,
+        accessSecret,
+    });
+}
 
 function canTweet() {
-    return consumer_key && consumer_secret && access_token_key && access_token_secret;
+    return appKey && appSecret && accessToken && accessSecret;
 }
 module.exports.canTweet = canTweet;
 
-function postMediaTweet(text, image) {
-    return new Promise((resolve, reject) => {
-        // Upload the image
-        client.post('media/upload', { media: image }, (error, media, response) => {
-            if (error)
-                return reject(error);
+async function postMediaTweet(text, image) {
+    let client = getClient();
 
-            let status = {
-                status: text,
-                media_ids: media.media_id_string,
-            };
+    // Upload images
+    let mediaId = await client.v1.uploadMedia(image, { mimeType: 'image/png' });
 
-            client.post('statuses/update', status, (error, tweet, response) => {
-                if (error)
-                    return reject(error);
-
-                resolve(tweet);
-            });
-        });
-    });
+    // Send status
+    await client.v2.tweet(text, { media: { media_ids: [mediaId] } });
 }
 module.exports.postMediaTweet = postMediaTweet;
