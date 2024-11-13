ac00c528f2657e0292dd75aa1611b8862f09d395
Matt Isenhower <matt@isenhower.com>
2024-11-12 19:27:50 -0800

    Update env var name

--- src/app/sync/S3Syncer.js
+++ src/app/sync/S3Syncer.js
@@ -39,7 +39,7 @@ class S3Syncer
   get s3Client() {
     return this._s3Client ??= new S3Client({
       endpoint: process.env.AWS_S3_ENDPOINT,
-      region: process.env.AWS_S3_REGION,
+      region: process.env.AWS_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
