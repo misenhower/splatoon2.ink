7d71c602544ea98f65af3d6ec346ed285450cf8d
Matt Isenhower <matt@isenhower.com>
2024-11-13 21:15:16 -0800

    Sync after generating Twitter images

--- src/app/twitter/twitter.js
+++ src/app/twitter/twitter.js
@@ -1,9 +1,16 @@
-const { canTweet } = require('./client');
+const S3Syncer = require('../sync/S3Syncer');
+const { canSync } = require('../sync');
 const tweets = require('./tweets');
 
 async function maybePostTweets() {
+    const syncer = canSync() ? new S3Syncer() : null;
+
     for (let tweet of tweets)
         await tweet.maybePostTweet();
+
+    if (syncer) {
+        await syncer.upload();
+    }
 }
 
 async function testScreenshots() {
